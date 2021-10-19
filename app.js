13364e0a9ee3e1349bbd07c54c427f2a03529b89
Mathias Dragovic <dragovic.mathias@gmail.com>
2021-10-19 13:56:57 +0200

    New todo is Input value

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function addTodo(event) {
     todoDiv.classList.add('todo');
     
     const newTodo = document.createElement ('li');
-    newTodo.innerText = "hey";
+    newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
     //check button
