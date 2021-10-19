ba1fd8a0d16fc3771dc197aaec23e481b4dfeb8b
Mathias Dragovic <dragovic.mathias@gmail.com>
2021-10-19 13:30:38 +0200

    Create LI on clic

--- app.js
+++ app.js
@@ -5,7 +5,34 @@ const todoList = document.querySelector('.todo-list');
 
 
 // Event listeners
-
+todoButton.addEventListener('click', addTodo);
 
 // Functions
+function addTodo(event) {
+    // Eviter de submit
+    event.preventDefault();
 
+    const todoDiv = document.createElement ('div');
+    todoDiv.classList.add('todo');
+    
+    const newTodo = document.createElement ('li');
+    newTodo.innerText = "hey";
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //check button
+    const completedButton = document.createElement ('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+    //trash button
+    const trashButton = document.createElement ('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+    // mettre dans ul
+    todoList.appendChild(todoDiv);
+
+
+
+
+}
